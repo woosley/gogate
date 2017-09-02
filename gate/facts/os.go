15fb8289e45890d4d69f08ca50cb3f1d98bea65a
woosley.xu <woosley.xu@gmail.com>
2017-09-02 17:01:01 +0800

    mac os

--- gate/facts/os.go
+++ gate/facts/os.go
@@ -3,15 +3,32 @@ package facts
 import (
 	"io/ioutil"
 	"os"
+	"runtime"
 )
 
 const redhat string = "/etc/redhat-release"
 
 func GetOs() string {
+	switch osf := runtime.GOOS; osf {
+	case "linux":
+		return getLinuxDistro()
+	case "darwin":
+		return getMacDistro()
+	default:
+		return "Unknow"
+	}
+}
+
+func getLinuxDistro() string {
+
 	if _, err := os.Stat(redhat); !os.IsExist(err) {
 		if bytes, err := ioutil.ReadFile("/etc/redhat-release"); err == nil {
 			return string(bytes)
 		}
 	}
 	return "Unknown"
+}
+
+func getMacDistro() string {
+	return "MacOS"
 }
